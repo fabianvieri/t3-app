73dd886a23f5335c6f2e7318f3c623ff543d3ad5
fabianvieri <vierifabian@gmail.com>
2024-04-19 22:54:17 +0700

    add basic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+const mockUrls = [
+  "https://utfs.io/f/0d6b6b2b-9ea8-4cd1-8660-d21afcd82633-3b6tbm.jpg",
+  "https://utfs.io/f/6e3c5e46-4cd4-4e9f-b64f-8b2bc3001186-boteja.jpg",
+  "https://utfs.io/f/03d68fa2-0ea5-4c9e-a6b7-e1c7805b1895-vm0eb5.jpg",
+  "https://utfs.io/f/ee7018b6-c841-468b-8adf-4749c02e613d-kduxvr.jpg",
+];
+
+const mockImages = mockUrls.map((url, i) => ({ id: i + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello app</h1>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map(({ id, url }) => (
+          <div className="w-full p-4 md:w-1/2 lg:w-1/3" key={id}>
+            <img src={url} alt={`Image ${id}`} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
