c520c59c299938f7c944d443c27374ef1f721d3a
fabianvieri <vierifabian@gmail.com>
2024-04-20 21:57:07 +0700

    add auth in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map(({ id, url, name }) => (
-          <div className="flex w-48 flex-col" key={id}>
-            <img src={url} alt={`Image ${id}`} />
-            <div>{name}</div>
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-wrap gap-4">
+      {images.map(({ id, url, name }) => (
+        <div className="flex w-48 flex-col" key={id}>
+          <img src={url} alt={`Image ${id}`} />
+          <div>{name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
